0b5731cc564695e355051c2922ff8b4036f81771
u10e10 <uplus.e10@gmail.com>
2016-09-04 03:09:56 +0900

    Multi button

--- tact-switch.py
+++ tact-switch.py
@@ -1,14 +1,20 @@
 import _webiopi.GPIO
-
 GPIO = _webiopi.GPIO
 
-DIGITAL_INPIN = 25
+# GPIO, status, current-status
+data = [[num, 0, False] for num in [17, 27, 22]]
 
-GPIO.setFunction(DIGITAL_INPIN, GPIO.IN)
+for n in data:
+    print(n[0], n[1], n[2])
+    GPIO.setFunction(n[0], GPIO.IN)
 
 while True:
-    bButtonState = GPIO.digitalRead(DIGITAL_INPIN)
-    if bButtonState:
-        while bButtonState:
-            bButtonState = GPIO.digitalRead(DIGITAL_INPIN)
-        print("clicked")
+    for n in data:
+        n[2] = GPIO.digitalRead(n[0])
+        if n[1] == 0 and n[2] == True:
+            n[1] = 1
+        elif n[1] == 1 and n[2] == False:
+            n[1] = 2
+        elif n[1] == 2:
+            n[1] = 0
+            print("%d clicked" % n[0])
