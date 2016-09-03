5cf9b594affce68e82749d78e45002427fd0a4f6
u10e10 <uplus.e10@gmail.com>
2016-09-04 02:52:33 +0900

    Not toggle

--- tact-switch.py
+++ tact-switch.py
@@ -4,14 +4,11 @@ GPIO = _webiopi.GPIO
 
 DIGITAL_INPIN = 25
 
-g_bButtonState_prev = False
-g_bLedState = False
-
 GPIO.setFunction(DIGITAL_INPIN, GPIO.IN)
 
 while True:
     bButtonState = GPIO.digitalRead(DIGITAL_INPIN)
-    if False == g_bButtonState_prev and True == bButtonState:
-        g_bLedState = not g_bLedState
-        print(g_bLedState)
-    g_bButtonState_prev = bButtonState
+    if bButtonState:
+        while bButtonState:
+            bButtonState = GPIO.digitalRead(DIGITAL_INPIN)
+        print("clicked")
