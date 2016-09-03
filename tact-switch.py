44a8514fbbc7218172c636b846b3ed189cd914b0
u10e10 <uplus.e10@gmail.com>
2016-09-04 02:38:03 +0900

    Print status instead of LED.

--- tact-switch.py
+++ tact-switch.py
@@ -3,7 +3,6 @@ import time
 
 GPIO = webiopi.GPIO
 
-LEDPIN = 24
 DIGITAL_INPIN = 25
 DEBOUNCE_DELAY = 0.5
 
@@ -11,9 +10,7 @@ g_bButtonState_prev = False
 g_bLedState = False
 g_dSec_prev = time.time()
 
-GPIO.setFunction(LEDPIN, GPIO.OUT)
 GPIO.setFunction(DIGITAL_INPIN, GPIO.IN)
-GPIO.digitalWrite(LEDPIN, g_bLedState)
 
 while True:
     bButtonState = GPIO.digitalRead(DIGITAL_INPIN)
@@ -21,6 +18,6 @@ while True:
         dSec = time.time()
         if DEBOUNCE_DELAY < dSec - g_dSec_prev:
             g_bLedState = not g_bLedState
-            GPIO.digitalWrite(LEDPIN, g_bLedState)
+            print(g_bLedState)
         g_dSec_prev = dSec
     g_bButtonState_prev = bButtonState
