8019e45127f0888027362d3fe037e22df4ea607d
u10e10 <uplus.e10@gmail.com>
2016-09-05 00:45:40 +0900

    Chattering guard.

--- tact-switch.py
+++ tact-switch.py
@@ -1,8 +1,9 @@
 import _webiopi.GPIO
+import time
 GPIO = _webiopi.GPIO
 
-# GPIO, status, current-status
-data = [[num, 0, False] for num in [17, 27, 22]]
+# GPIO, status, time
+data = [[num, 0, 0.0] for num in [17, 27, 22]]
 
 for n in data:
     print(n[0], n[1], n[2])
@@ -13,8 +14,9 @@ while True:
         status = GPIO.digitalRead(n[0])
         if n[1] == 0 and status:
             n[1] = 1
+            n[2] = time.time()
         elif n[1] == 1 and not status:
             n[1] = 2
-        elif n[1] == 2:
+        elif n[1] == 2 and 0.2 < (time.time() - n[2]):
             n[1] = 0
             print("%d clicked" % n[0])
