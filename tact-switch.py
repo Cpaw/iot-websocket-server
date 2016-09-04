963a0bb13e0ec11a18b79f37dc79058937ef0e07
u10e10 <uplus.e10@gmail.com>
2016-09-05 01:04:18 +0900

    Simply

--- tact-switch.py
+++ tact-switch.py
@@ -1,23 +1,17 @@
-import webiopi
-import time
+import _webiopi.GPIO
 
-GPIO = webiopi.GPIO
+GPIO = _webiopi.GPIO
 
 DIGITAL_INPIN = 25
-DEBOUNCE_DELAY = 0.5
 
 g_bButtonState_prev = False
 g_bLedState = False
-g_dSec_prev = time.time()
 
 GPIO.setFunction(DIGITAL_INPIN, GPIO.IN)
 
 while True:
     bButtonState = GPIO.digitalRead(DIGITAL_INPIN)
     if False == g_bButtonState_prev and True == bButtonState:
-        dSec = time.time()
-        if DEBOUNCE_DELAY < dSec - g_dSec_prev:
-            g_bLedState = not g_bLedState
-            print(g_bLedState)
-        g_dSec_prev = dSec
+        g_bLedState = not g_bLedState
+        print(g_bLedState)
     g_bButtonState_prev = bButtonState
